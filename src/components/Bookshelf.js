2697ca1340185b1e9bf1512105b60d0e66c4aca8
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-07-30 23:20:09 +0530

    correct key error
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- src/components/Bookshelf.js
+++ src/components/Bookshelf.js
@@ -7,9 +7,9 @@ const Bookshelf = props => {
 			<h2 className="bookshelf-title">{props.title}</h2>
 			<div className="bookshelf-books">
 				<ol className="books-grid">
-					{props.books.map((book, index) => (
-						<li>
-							<Book key={index} book={book} movebook={props.movebook} />
+					{props.books.map(book => (
+						<li key={book.id}>
+							<Book book={book} movebook={props.movebook} />
 						</li>
 					))}
 				</ol>
